8fc3899846132ce2b6439e1f3582bd4b45c06139
Gurvesh <guru01@gmail.com>
2016-12-30 07:30:41 +0000

    Continues attempt at palette

--- src/nature_code/chap1/gaussian_color_palette.clj
+++ src/nature_code/chap1/gaussian_color_palette.clj
@@ -4,11 +4,8 @@
 (def r-generator (new java.util.Random))
 
 (defn setup []
-  (q/background 255)
+  (q/background 0)
   (q/no-stroke))
-
-(defn get-color [theta]
-  (cond ))
 
 (defn draw []
   (let [num (float (. r-generator nextGaussian))
@@ -18,8 +15,14 @@
         theta (q/random (* 2 (Math/PI)))
         x (+ 320 (* r (Math/sin theta)))
         y (+ 320 (* r (Math/cos theta)))
-        color (get-color theta)]
-    (q/fill 255, 0, 0, 10)
+        theta1 (rem (+ theta (* 2/3 (Math/PI)))
+                    (* 2 (Math/PI)))
+        theta2 (rem (+ theta (* 4/3 (Math/PI)))
+                    (* 2 (Math/PI)))
+        r (int (* 255 (/ theta (* 2 (Math/PI)))))
+        g (int (* 255 (/ theta1 (* 2 (Math/PI)))))
+        b (int (* 255 (/ theta2 (* 2 (Math/PI)))))]
+    (q/fill r, g, b, 10)
     (q/ellipse x, y, 16, 16)))
 
 (q/defsketch example
